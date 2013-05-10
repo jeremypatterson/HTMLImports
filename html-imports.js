3d0ac6f3a3c87f8ee0328edb9d89d37388a9af59
Yvonne Yip <ykyyip@google.com>
2013-05-10 11:33:50 -0700

    move inline script to file

--- html-imports.js
+++ html-imports.js
@@ -29,6 +29,7 @@ if (!window.Loader) {
   var path = basePath + 'tools/loader/loader.js';
   document.write('<script src="' + path + '"></script>');
 } 
-document.write('<script>Loader.load("' + scopeName + '")</script>');
+//document.write('<script>Loader.load("' + scopeName + '")</script>');
+document.write('<script src="' + basePath + 'run-loader.js"></script>');
   
 })();
